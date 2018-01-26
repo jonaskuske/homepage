e96f08afd2f0181b81d2f5268e44ca68e24e7c47
Jonas <30421456+jonaskuske@users.noreply.github.com>
2018-01-26 18:13:54 +0100

    open links in new tab

--- website/src/components/HeaderLink.jsx
+++ website/src/components/HeaderLink.jsx
@@ -1,5 +1,5 @@
 export default (props, children) => (
-  <a style={{ marginLeft: '1.5rem' }} href={props.href || '#'} >
+  <a style={{ marginLeft: '1.5rem' }} target='_blank' rel='noopener' href={props.href || '#'} >
     <p class={'header-link-text ' + props.class}> {children || 'Platzhalter'}</p>
   </a>
 );
