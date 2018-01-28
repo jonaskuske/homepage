e9bc4dff31e993bbd1e04785bf933875730ac4ee
Jonas <30421456+jonaskuske@users.noreply.github.com>
2018-01-28 08:56:45 +0100

    configured CleanWebpackPlugin

--- website/webpack.config.js
+++ website/webpack.config.js
@@ -3,6 +3,7 @@
 const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: ['promise-polyfill/src/polyfill', 'whatwg-fetch', './src/main.js'],
@@ -47,6 +48,7 @@ module.exports = {
     ]
   },
   plugins: [
+    new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'Portfolio | Jonas Kuske',
       minify: {
