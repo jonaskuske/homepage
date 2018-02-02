9f50dedc35ae862a27389f88d31789eec7032ef0
Jonas <30421456+jonaskuske@users.noreply.github.com>
2018-02-02 05:33:21 +0100

    LoadingScreen fallback for Edge

--- website/src/components/LoadingScreen.jsx
+++ website/src/components/LoadingScreen.jsx
@@ -1,8 +1,15 @@
 import Spinner from '@@/LoadingSpinner';
+import { svgAnimation } from '@/lib/browser-support';
 
 const view = ({ data: { color }, class: className = '', ...props }) => (
   <div class={`${className} loading-screen `} {...props}>
-    <Spinner class='main-spinner' color={{ stroke: color }} />
+    {svgAnimation
+      ? < Spinner class='main-spinner' color={{ stroke: color }} />
+      : (
+        < div class='animation-fallback main-spinner'>
+          <Spinner animation={{ inner: false, outer: false }} color={{ stroke: color }} />
+        </div>)
+    }
   </div>
 );
 
