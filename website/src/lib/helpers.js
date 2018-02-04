c95e5c9fb052ef8c6e64690574350f2ba072c01d
Jonas <30421456+jonaskuske@users.noreply.github.com>
2018-02-04 07:15:44 +0100

    Added helper functions for error handling

--- website/src/lib/helpers.js
+++ website/src/lib/helpers.js
@@ -1,1 +1,4 @@
 export const wait = time => new Promise(resolve => { setTimeout(resolve, time); });
+export let errorMessage = '';
+export const log = e => (errorMessage = e);
+export const error = e => { throw new Error(e); };
