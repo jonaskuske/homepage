bfc57855dd646fc6053218f97f16b1d606401fc6
Jonas <30421456+jonaskuske@users.noreply.github.com>
2018-02-08 11:16:15 +0100

    href to GitHub adjusted

--- website/src/components/Projects.jsx
+++ website/src/components/Projects.jsx
@@ -10,7 +10,7 @@ const view = ({ data: { projects, color, mobile }, class: className = '', ...pro
   return (
     <main key='projects' class={className} {...props}>
       <h1>PROJEKTE</h1>
-      <p> Neben den hier vorgestellten Arbeiten ist natürlich auch die Portfolio-Seite, die Sie gerade betrachten, eines meiner Projekte. Geschrieben ist sie mit Hilfe des JavaScript Frameworks »hyperapp«, einem minimalistischen Framework, das in seiner Funktionsweise React &auml;hnelt. Außerdem wurde sie unter Zuhilfenahme modernster Browser-Technologien wie CSS Variablen oder CSS Grid gebaut. Der Quellcode ist <a href='https://github.com/jonaskuske/'>hier</a> zu sehen. <br />Viel Spaß beim Erkunden! </p>
+      <p> Neben den hier vorgestellten Arbeiten ist natürlich auch die Portfolio-Seite, die Sie gerade betrachten, eines meiner Projekte. Geschrieben ist sie mit Hilfe des JavaScript Frameworks »hyperapp«, einem minimalistischen Framework, das in seiner Funktionsweise React &auml;hnelt. Außerdem wurde sie unter Zuhilfenahme modernster Browser-Technologien wie CSS Variablen oder CSS Grid gebaut. Der Quellcode ist <a href='https://github.com/jonaskuske/portfolio'>hier</a> zu sehen. <br />Viel Spaß beim Erkunden! </p>
       <br />
       <section class='projekt-container'>
         {werke.reverse().map(({ title, subtitle, id, category, image }) => {
