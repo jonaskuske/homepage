d375f788001aa2e9eb7ffee97d67c790d969206e
Jonas <30421456+jonaskuske@users.noreply.github.com>
2018-02-11 22:25:22 +0100

    error handling for image loader

--- website/src/lib/image-loader.js
+++ website/src/lib/image-loader.js
@@ -1,19 +1,20 @@
-const imageLoader = images => new Promise(resolve => {
+const imageLoader = (images, strict = false) => new Promise((resolve, reject) => {
   if (typeof images === 'undefined') return resolve();
   typeof images === 'string'
-    ? loadImage(images).then(resolve)
-    : loadAllImages(images).then(resolve);
+    ? loadImage(images, strict).then(resolve, reject)
+    : loadAllImages(images, strict).then(resolve, reject);
 });
 
-function loadAllImages(imageList) {
-  const imageLoadPromises = imageList.map(img => loadImage(img));
+function loadAllImages(imageList, strict) {
+  const imageLoadPromises = imageList.map(img => loadImage(img, strict));
   return Promise.all(imageLoadPromises);
 }
 
-function loadImage(img) {
-  return new Promise(resolve => {
+function loadImage(img, strict) {
+  return new Promise((resolve, reject) => {
     const el = new Image();
     el.onload = () => resolve(el);
+    el.onerror = e => strict ? reject(e) : resolve(e);
     el.src = img;
   });
 }
