305a935acfeb58aec449f27e74cc1353af0cd591
Jonas <30421456+jonaskuske@users.noreply.github.com>
2018-02-19 17:10:52 +0100

    project list now an array, unneccessary props removed

--- website/src/assets/projekte.js
+++ website/src/assets/projekte.js
@@ -1,42 +1,38 @@
-import serpensPro from '@img/thumbnails/serpens-pro.jpg';
-import typro from '@img/thumbnails/typro.png';
-import dmpPortfolio from '@img/thumbnails/dmp-portfolio.jpg';
-import lsbHomepage from '@img/thumbnails/lsb-homepage.png';
+import serpensPro from '@projects/serpens-pro/serpens-pro.jpg';
+import typro from '@projects/typro/typro.jpg';
+import dmpPortfolio from '@projects/dmp-portfolio/dmp-portfolio.jpg';
+import lsbHomepage from '@projects/lsb-homepage/lsb-homepage.jpg';
 
-export default {
-  'serpens-pro': {
+export default [
+  {
     title: '»Serpens Pro«',
     subtitle: 'Elementarfont',
     category: 'Grafikdesign',
     id: 'serpens-pro',
     highlight: true,
-    image: serpensPro,
-    text: 'Ein geometrischer Font, basierend aus geachtelten Kreisen in zwei Radien und Geraden in korrespondierender Länge. Lesbarkeit stand bei diesem Projekt im Hintergrund, viel mehr ging es darum, klare Regeln zu definieren, anhand derer die Schriftarten konstruiert werden sollte, und nicht von diesen abzuweichen, selbst wenn es der Lesbarkeit schadet.'
+    image: serpensPro
   },
-  'typro': {
+  {
     title: '»typro«',
     subtitle: 'Progressive Web App',
     category: 'Konzept, Design, Development',
     id: 'typro',
     highlight: true,
-    image: typro,
-    text: 'Eine Progressive Web App, die es dem Nutzer erlaubt, mit Hilfe seiner Smartphone-Kamera Schriftarten zu identifizieren und zu speichern. [...]'
+    image: typro
   },
-  'dmp-portfolio': {
+  {
     title: '»Digitale Medienproduktion«',
     subtitle: 'Portfolio',
     category: 'Full-Stack Webprojekt',
     id: 'dmp-portfolio',
     highlight: true,
-    image: dmpPortfolio,
-    text: 'Portfolio Website für den Studiengang »Digitale Medienproduktion« der Hochschule Bremerhaven. Hier werden studentische Arbeiten präsentiert. [...]'
+    image: dmpPortfolio
   },
-  'lsb-homepage': {
+  {
     title: '»Landessportbund Bremen«',
     subtitle: 'Homepage',
     category: 'Webdesign & Prototyp',
     id: 'lsb-homepage',
-    image: lsbHomepage,
-    text: 'Prototyp einer redesignten Homepage, entworfen im Rahmen eines Corporate Design Konzepts für den Landessportbund Bremen. [...]'
+    image: lsbHomepage
   }
-};
+];
