f3d1e753a221d18d61642851dd8993155ed67c09
Jonas <30421456+jonaskuske@users.noreply.github.com>
2018-02-24 01:23:09 +0100

    jsdoc type annotations added

--- website/src/lib/helpers.js
+++ website/src/lib/helpers.js
@@ -1,14 +1,54 @@
 /* global h */
+
+/**
+ * Promise that resolves after a given time
+ *
+ * @param {number} time time in milliseconds
+ */
+
 export const wait = time => new Promise(resolve => { setTimeout(resolve, time); });
+
+/**
+ * Takes a max value and returns a random integer between 0 and that value
+ *
+ * @param {number} max The maximal value the returned random integer should have
+ *
+ * @returns {number} A random number between 0 and the given max limit
+ */
 export const random = max => Math.floor(Math.random() * Math.floor(max));
+
+/**
+ * @type {Array.<Error>} Array containing all errors logged using helper log()
+ */
 export const errorMessages = [];
+
+/**
+ * Pushes Error to helper "errorMessages" and logs to console
+ *
+ * @param {Error} e
+ */
 export const log = e => {
   errorMessages.push(e);
   console.error(e);
 };
+
+/**
+ * Throws a new Error with a given error message
+ *
+ * @param {string} e error message
+ */
 export const error = e => { throw new Error(e); };
-export const parseText = str => {
-  const children = str.split(/(?=<a.+<\/a>)|<\/a>|(?=<br)|r\s?\/>/);
+
+
+/**
+ * Transforms simple HTML strings to arrays, where inlined linebreaks and links
+ * in the form of <a>...</a> and <br/> (needs closing slash!) are replaced by VNodes returned from h()
+ *
+ * @param {string} html HTML string, may contain <br/> and <a> tags.
+ *
+ */
+export const parseText = html => {
+  const children = html.split(/(?=<a.+<\/a>)|<\/a>|(?=<br)|r\s?\/>/);
 
   const parsed = children.map(el => {
     if (el.indexOf('<b') === 0) return h('br');
@@ -20,7 +60,7 @@ export const parseText = str => {
     const inner = el.replace(/<a\s|>.+/g, '');
     const pairs = inner.split(' ');
     pairs.forEach(pair => {
-      const [key, val = true] = pair.split(/=(.+)/);
+      const [key, val = 'true'] = pair.split(/=(.+)/);
       attributes[key] = JSON.parse(val);
     });
 
