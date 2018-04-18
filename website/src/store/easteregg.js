71195e17720fd609b4f608d965388d464d92c05e
Jonas <30421456+jonaskuske@users.noreply.github.com>
2018-04-18 23:37:30 +0200

    feat: add more colors when starting easteregg

--- website/src/store/easteregg.js
+++ website/src/store/easteregg.js
@@ -1,4 +1,18 @@
 let beforeEastereggColor = '';
+const addPartyColors = (() => {
+  let added = false;
+
+  return actions => {
+    if (added) return;
+    actions.addColor('#000000');
+    actions.addColor('#006200');
+    actions.addColor('#ff3edd');
+    actions.addColor('#f3ff0d');
+    actions.addColor('#dd0000');
+    actions.addColor('#ffe42b');
+    added = true;
+  };
+})();
 
 export const eastereggState = {
   eastereggActive: false,
@@ -7,6 +21,7 @@ export const eastereggState = {
 
 export const eastereggActions = {
   toggleEasteregg: () => (state, actions) => {
+    addPartyColors(actions);
     if (state.eastereggActive) {
       clearInterval(state.eastereggInterval);
       actions.setMetaTagColor(true);
@@ -17,7 +32,7 @@ export const eastereggActions = {
       actions.setMetaTagColor(false);
       return {
         eastereggActive: true,
-        eastereggInterval: setInterval(actions.setRandomColor, 200)
+        eastereggInterval: setInterval(actions.setRandomColor, 185)
       };
     }
   }
