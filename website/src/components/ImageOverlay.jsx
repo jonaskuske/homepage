b81c41cc137022d8d8be16de1c6a6a94308a8a4c
Jonas <30421456+jonaskuske@users.noreply.github.com>
2018-04-29 02:23:43 +0200

    feat: enable object-fit polyfill for legacy browsers

--- website/src/components/ImageOverlay.jsx
+++ website/src/components/ImageOverlay.jsx
@@ -1,12 +1,17 @@
 import actions from '@/main';
 import { wait } from '@/lib/helpers';
+import { objectFitSupported } from '@/lib/browser-support';
+import objectFitPolyfill from 'object-fit-images';
 
 const animateIn = el => wait(30).then(() => el.classList.remove('invisible'));
 const animateOut = (el, done) => {
   el.classList.add('invisible');
   wait(300).then(done);
 };
-const animateImg = el => wait(30).then(() => el.classList.remove('overlay-squish'));
+const showImage = el => {
+  !objectFitSupported && objectFitPolyfill(el);
+  wait(30).then(() => el.classList.remove('overlay-squish'));
+};
 
 const view = ({ src }) => (
   <div
@@ -16,7 +21,7 @@ const view = ({ src }) => (
     oncreate={animateIn}
     onremove={animateOut}
   >
-    <img class='overlay-squish' oncreate={animateImg} src={src} alt="" />
+    <img class='overlay-squish' oncreate={showImage} src={src} alt="" />
   </div>
 );
 
--- website/src/pages/DetailPage.jsx
+++ website/src/pages/DetailPage.jsx
@@ -1,15 +1,15 @@
 import { parseText } from '@/lib/helpers';
 import { ImageClickable, ImageTextBlock } from '@/components';
 
-const view = ({ data: { project: { title, image, text, showcase, blocks, footer } }, class: className = '', ...props }) => (
+const view = ({ data: { project: { title, image, text, showcase, blocks, footer }, mobile }, class: className = '', ...props }) => (
   <main key='details' class={`${className}`} {...props} >
     <h1>{title.toUpperCase()}</h1>
     <section class='detail-container'>
-      <ImageClickable class='detail-image' src={showcase.image} />
+      <ImageClickable class='detail-image' mobile={mobile} src={showcase.image} />
       <p class='detail-text showcase textsquish-blocker'> {parseText(showcase.text)} </p>
       {blocks.map(({ image, text }, index) => {
         const mode = index % 2 === 0 ? 'left' : 'right';
-        return <ImageTextBlock src={image} mode={mode}>{text}</ImageTextBlock>;
+        return <ImageTextBlock src={image} mode={mode} mobile={mobile}>{text}</ImageTextBlock>;
       })}
       {footer && <p class="detail-text textsquish-blocker"> {parseText(footer)} </p>}
     </section>
