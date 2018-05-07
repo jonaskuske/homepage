7c1386cf7012a003a448dae285230ba1bc330a69
Jonas <30421456+jonaskuske@users.noreply.github.com>
2018-05-07 16:52:40 +0200

    fix: sw registration only in prod environment

--- website/src/main.js
+++ website/src/main.js
@@ -30,13 +30,3 @@ else vm.setRandomColor();
 vm.getLanguage({ language })
   .then(vm.fetchProjects)
   .then(router.init);
-
-
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js').catch(err => {
-    console.warn('SW registration failed: ', err);
-  });
-}
-
-// If deployed as PWA: hide web app install banner (inappropriate for portfolio site)
-window.addEventListener('beforeinstallprompt', e => e.preventDefault());
