64a11165208694eed112a40e7ce14ecdc2f8d450
Jonas <30421456+jonaskuske@users.noreply.github.com>
2018-05-11 18:36:43 +0200

    fix: add aria-label for a11y

--- website/src/components/ColorPicker.jsx
+++ website/src/components/ColorPicker.jsx
@@ -3,6 +3,7 @@ import actions from '@/main';
 const view = props => (
   <input
     type='color'
+    aria-label='Select theme color for the site'
     onchange={({ target: { value } }) => {
       actions.setColor(value);
       actions.addColor(value);
