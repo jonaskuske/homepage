a7daa80308eb45f1d3a01f9cface8e4e7a71d4f6
Jonas Kuske <30421456+jonaskuske@users.noreply.github.com>
2018-09-23 23:18:21 +0200

    refactor: use namespaced websocket

--- website/src/pages/PhonePairPage.jsx
+++ website/src/pages/PhonePairPage.jsx
@@ -1,45 +1,65 @@
-import actions from '@/main';
-import { domainExtension, parseText } from '@/lib/helpers';
-import QRCode from 'qrcode';
-import io from 'socket.io-client';
+import actions from '@/main'
+import { domainExtension, parseText } from '@/lib/helpers'
+import QRCode from 'qrcode'
+import io from 'socket.io-client'
 
 const socket = (() => {
-  let webSocket = false;
+  let webSocket = false
   return () => {
-    if (webSocket) return webSocket;
+    if (webSocket) return webSocket
     else {
-      const socket = io('https://data.jonaskuske.com');
+      const socket = io('https://data.jonaskuske.com/colorservice')
       socket.on('connection_established', () => {
-        actions.setSessionID(socket.id);
-      });
+        actions.setSessionID(socket.id)
+      })
       socket.on('set_color', ({ color }) => {
-        actions.setColor(color);
-      });
-      return webSocket = socket;
+        actions.setColor(color)
+      })
+      return (webSocket = socket)
     }
-  };
-})();
+  }
+})()
 
-const createQRCode = (el, id, mobile) => QRCode.toCanvas(
-  el,
-  `https://jonaskuske.${domainExtension || 'com'}/colorpicker?session=${id}`,
-  { margin: 2, width: mobile ? 200 : 400 }
-);
+const createQRCode = (el, id, mobile) =>
+  QRCode.toCanvas(
+    el,
+    `https://jonaskuske.${domainExtension || 'com'}/colorpicker?session=${id.replace(
+      '/colorservice#',
+      '',
+    )}`,
+    {
+      margin: 2,
+      width: mobile ? 200 : 400,
+    },
+  )
 
-const view = ({ data: { locales: { PairYourPhone = {} }, sessionID: id, mobile }, class: className = '', ...props }) => (
+const view = ({
+  data: {
+    locales: { PairYourPhone = {} },
+    sessionID: id,
+    mobile,
+  },
+  class: className = '',
+  ...props
+}) => (
   <main {...props} class={`phone-pair-page ${className}`} key="pair" oncreate={socket}>
     <h1>{PairYourPhone.h1}</h1>
-    {!id
-      ? <p>{PairYourPhone.connecting}</p>
-      : [
+    {!id ? (
+      <p>{PairYourPhone.connecting}</p>
+    ) : (
+      [
         <p>{parseText(PairYourPhone.description)}</p>,
         <canvas
           oncreate={el => createQRCode(el, id, mobile)}
           onupdate={el => createQRCode(el, id, mobile)}
         />,
-        <p>{PairYourPhone.sessionID}<span style={{ fontWeight: 600 }}>{id}</span></p>
-      ]}
+        <p>
+          {PairYourPhone.sessionID}
+          <span style={{ fontWeight: 600 }}>{id.replace('/colorservice#', '')}</span>
+        </p>,
+      ]
+    )}
   </main>
-);
+)
 
-export default view;
+export default view
