659bcb20887f6b49c048958cf66c6ab5404c5101
Jonas Kuske <30421456+jonaskuske@users.noreply.github.com>
2018-10-26 07:27:05 +0200

    [website] fix: use proper flexbox layout in project detail view

--- website/src/components/ImageClickable.jsx
+++ website/src/components/ImageClickable.jsx
@@ -1,29 +1,28 @@
-import actions from '@/main';
-import { objectFitSupported, isFirefox } from '@/lib/browser-support';
-import { withBlur } from '@/lib/helpers';
-import objectFitPolyfill from 'object-fit-images';
+import actions from '@/main'
+import { objectFitSupported, isFirefox } from '@/lib/browser-support'
+import { withBlur } from '@/lib/helpers'
+import objectFitPolyfill from 'object-fit-images'
 
 const openOverlay = evt => {
-  const image = evt.target.src;
-  actions.setOverlayImage(image);
-  actions.showOverlay();
-};
+  const image = evt.target.src
+  actions.setOverlayImage(image)
+  actions.showOverlay()
+}
 
 const view = ({ class: className = '', mobile, ...props }) => (
   <img
     class={`pointer clickable-img ${className}`}
-    style={!mobile && (isFirefox || !objectFitSupported) && { minWidth: '500px' }}
     oncreate={!objectFitSupported && objectFitPolyfill}
     tabindex={0}
     onclick={withBlur(openOverlay)}
     onkeydown={e => {
       if (e.keyCode === 13 || e.keyCode === 32) {
-        e.preventDefault();
-        openOverlay(e);
+        e.preventDefault()
+        openOverlay(e)
       }
     }}
     {...props}
   />
-);
+)
 
-export default view;
+export default view
