dbedb23ed319382b1ad49752375dff8b9c713ff7
Jonas Kuske <30421456+jonaskuske@users.noreply.github.com>
2018-12-03 23:23:40 +0100

    [website] refactor: add more comments to color section of store :)

--- website/src/store/colors.js
+++ website/src/store/colors.js
@@ -1,11 +1,11 @@
 import { random, throttle } from '@/lib/helpers'
-import { cssVariables } from '@/lib/browser-support'
+import { cssVariables as supportsCssVariables } from '@/lib/browser-support'
 import cssVarPolyfill from 'css-vars-ponyfill'
 
 const additonalColors = ['#77f113', '#ffff00', '#f58b00', '#800080', '#CB1633', '#b815ef']
 let additionalColorsAdded = false
-
-!cssVariables && cssVarPolyfill()
+/* Run CSS var polyfill once on startup to update all CSS vars in IE */
+if (!supportsCssVariables) cssVarPolyfill()
 
 const colorMetaTag = document.querySelector('meta[name=theme-color]')
 const throttledCssVarPolyfill = throttle(cssVarPolyfill, 1200)
@@ -19,6 +19,7 @@ export const colorState = {
 export const colorActions = {
   /* Update CSS variables, HTML meta tag and store with the new theme color */
   setColor: themeColor => (state, actions) => {
+    /* Passed themeColor is not valid? Just set a random one :) */
     if (!(themeColor.includes('#') && themeColor.length === 7)) return actions.setRandomColor()
 
     let safeThemeColor = themeColor
@@ -33,7 +34,7 @@ export const colorActions = {
     /* Create semi-transparent version so the background image is visible through the color layer */
     const bgColor = `rgba(${r},${g},${b},0.6)`
 
-    /* Set the safeThemeColor to white if themeColor is too dark, ensures good visibilty of elements */
+    /* Set safeThemeColor to white if themeColor is too dark, ensures good visibilty of elements */
     if (brightness < 60) safeThemeColor = '#ffffff'
 
     document.documentElement.style.setProperty('--bg-color', bgColor)
@@ -41,7 +42,8 @@ export const colorActions = {
     document.documentElement.style.setProperty('--theme-color', safeThemeColor)
     if (state.updateMetaTagColor) colorMetaTag.content = themeColor
 
-    !cssVariables &&
+    // If necessary: Use css-vars-ponyfill to update vars in IE, throttled for stable performance
+    if (!supportsCssVariables) {
       throttledCssVarPolyfill({
         variables: {
           'bg-color': bgColor,
@@ -50,23 +52,27 @@ export const colorActions = {
         },
         onlyVars: true,
       })
-
+    }
     return { safeThemeColor, themeColor }
   },
-  /* Set a random color from the colors array that isn't the already set color */
+  /* Set a random color from the colors array that isn't the current color */
   setRandomColor: () => (state, actions) => {
     const colorOptions = state.colors.filter(color => color !== state.themeColor)
     const randomColor = colorOptions[random(colorOptions.length)]
     actions.setColor(randomColor)
+    /* After requesting a random color for the first time:
+      Add additional colors to array. User is asking for it, so ready for experimental stuff :P */
     if (!additionalColorsAdded) {
       additonalColors.forEach(actions.addColor)
       additionalColorsAdded = true
     }
   },
   /* Add color to colors array if it isn't included already */
-  addColor: newColor => ({ colors }) =>
-    !colors.includes(newColor) &&
-    newColor.includes('#') &&
-    newColor.length === 7 && { colors: [...colors, newColor] },
+  addColor: newColor => ({ colors }) => {
+    if (!colors.includes(newColor) && newColor.includes('#') && newColor.length === 7) {
+      return { colors: [...colors, newColor] }
+    }
+  },
+  /* Toggle whether the meta tag (affecting browser chrome etc.) is updated along side themecolor */
   setMetaTagColor: val => ({ updateMetaTagColor: val }),
 }
